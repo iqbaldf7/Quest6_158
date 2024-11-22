a6ea5fdda60f91ba6f12706269ee992d6eccdbbd
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-11-22 15:17:01 +0700

    Menambah data class Mahasiswa

--- app/src/main/java/com/example/activity6/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/activity6/ui/viewmodel/MahasiswaViewModel.kt
@@ -1,25 +1,52 @@
-package com.example.activity6.ui.viewmodel
+package com.example.praktikum6.ui.view.viewmodel
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.example.activity6.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class Mahasiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val email: String = ""
+)
+
 
 class MahasiswaViewModel : ViewModel() {
-    private val mahasiswaStateUI = MutableStateFlow(Mahasiswa())
+    private val _mhsState = MutableStateFlow(Mahasiswa())
+    val mhsStateUi: StateFlow<Mahasiswa> = _mhsState.asStateFlow()
 
-    val mahasiswaState: StateFlow<Mahasiswa> = mahasiswaStateUI.asStateFlow()
-    fun saveDataMahasiswa(ls: MutableList<String>){
-        mahasiswaStateUI.value = Mahasiswa(
-            nim = ls[0],
-            nama = ls[1],
-            email = ls[2]
-
-
-        )
-
+    fun setNim(inputNim: String) {
+        _mhsState.update { state ->
+            state.copy(nim = inputNim)
+        }
     }
 
+    fun setNama(inputNama: String) {
+        _mhsState.update { state ->
+            state.copy(nama = inputNama)
+        }
+    }
+
+    fun setEmail(inputEmail: String) {
+        _mhsState.update { state ->
+            state.copy(email = inputEmail)
+        }
+    }
+
+    fun saveDataMhs(ls: MutableList<String>) {
+        if (ls.size >= 3) { // Validasi panjang list
+            _mhsState.update { state ->
+                state.copy(
+                    nim = ls[0],
+                    nama = ls[1],
+                    email = ls[2]
+                )
+            }
+        } else {
+            throw IllegalArgumentException("List must have at least 3 elements")
+        }
+    }
 }
+

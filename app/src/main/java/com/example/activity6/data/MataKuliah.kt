16468ccd4591913ebfe905c73001e9f1b9f1a946
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-11-22 13:49:19 +0700

    menambah list objek dar MataKuliah

--- app/src/main/java/com/example/activity6/data/MataKuliah.kt
+++ app/src/main/java/com/example/activity6/data/MataKuliah.kt
@@ -1,4 +1,14 @@
 package com.example.activity6.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision",
+    )
 }
+
+

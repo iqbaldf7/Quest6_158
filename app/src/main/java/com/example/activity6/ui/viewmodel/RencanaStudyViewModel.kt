8630302cb6b9ef138c5b77f919969a13ed935318
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-11-22 15:35:47 +0700

    Menambah data cass rencanastudi untuk data KRS

--- app/src/main/java/com/example/activity6/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/activity6/ui/viewmodel/RencanaStudyViewModel.kt
@@ -1,12 +1,39 @@
-package com.example.activity6.ui.viewmodel
+package com.example.praktikum6.ui.view.viewmodel
 
-import com.example.activity6.model.RencanaStudi
+import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-class RencanaStudyViewModel() {
-    private  val _krsState = MutableStateFlow(RencanaStudi())
-    val krsStudi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+data class RencanaStudi(
+    val namaMK: String = "",
+    val kelas: String = ""
+)
 
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        if (ls.size >= 2) {
+            _krsState.update { status ->
+                status.copy(
+                    namaMK = ls[0],
+                    kelas = ls[1]
+                )
+            }
+        } else {
+            throw IllegalArgumentException("List must contain at least 2 elements")
+        }
+    }
 }
